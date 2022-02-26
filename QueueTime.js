b624e9d825f0a211268ebbda6bcaad93c34ad7e5
machinemessiah <mark.edward.franklin@gmail.com>
2022-02-26 02:11:57 -0500

    Optional chaining added
    Changed `style` to `qt_style` in case any other scripts initialize a var with the same name.
    Added optional chaining when grabbing `Spicetify.Queue.nextTracks` and when attempting to update the css var so errors aren't spit out when not on the queue page (the errors weren't causing problems, but cleaner to just prevent them anyway).

--- QueueTime.js
+++ QueueTime.js
@@ -1,12 +1,12 @@
-let style = document.createElement( "style" );
-style.innerHTML = `
+let qt_style = document.createElement( "style" );
+qt_style.innerHTML = `
 h2.OSrSKCORKBO4JZclYmQ5.standalone-ellipsis-one-line::after {
     content: var(--queue-remaining);
     float: right;
     color: gray;
 }
 `;
-document.head.appendChild( style );
+document.head.appendChild( qt_style );
 
 let momentScript = document.createElement( "script" );
 momentScript.setAttribute( 'src', 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js' );
@@ -17,9 +17,9 @@ document.head.appendChild( momentScript );
 
 setInterval( function () {
 	let totalTime = 0;
-	Spicetify.Queue.nextTracks.some( t => {
+	Spicetify.Queue?.nextTracks.some( t => {
 		if ( isNaN( Number( t.contextTrack.metadata.duration ) ) ) return true;
 		totalTime += Number( t.contextTrack.metadata.duration );
 	} );
-	document.querySelector( `h2.OSrSKCORKBO4JZclYmQ5` ).style.setProperty( '--queue-remaining', `'${moment.utc( totalTime + Spicetify.Player.getDuration() - Spicetify.Player.getProgress() ).format( 'HH:mm:ss' )} Remaining'` );
+	document.querySelector( `h2.OSrSKCORKBO4JZclYmQ5` )?.style.setProperty( '--queue-remaining', `'${moment.utc( totalTime + Spicetify.Player.getDuration() - Spicetify.Player.getProgress() ).format( 'HH:mm:ss' )} Remaining'` );
 }, 1000 );
