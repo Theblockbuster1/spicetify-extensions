48ee9ce756e6886b61c686c9840474432f86e71c
Theblockbuster1 <48650812+Theblockbuster1@users.noreply.github.com>
2022-05-29 19:21:04 +0100

    Update element selector & update moment.js

--- QueueTime.js
+++ QueueTime.js
@@ -1,6 +1,6 @@
 let qt_style = document.createElement( "style" );
 qt_style.innerHTML = `
-h2.OSrSKCORKBO4JZclYmQ5.standalone-ellipsis-one-line::after {
+h2.jf2HafzDEI9jn7Yo05eM.standalone-ellipsis-one-line::after {
     content: var(--queue-remaining);
     float: right;
     color: gray;
@@ -9,8 +9,8 @@ h2.OSrSKCORKBO4JZclYmQ5.standalone-ellipsis-one-line::after {
 document.head.appendChild( qt_style );
 
 let momentScript = document.createElement( "script" );
-momentScript.setAttribute( 'src', 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js' );
-momentScript.setAttribute( 'integrity', 'sha512-qTXRIMyZIFb8iQcfjXWCO8+M5Tbc38Qi5WzdPOYZHIlZpzBHG3L3by84BBBOiRGiEb7KKtAOAs5qYdUiZiQNNQ==' );
+momentScript.setAttribute( 'src', 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.3/moment.min.js' );
+momentScript.setAttribute( 'integrity', 'sha512-x/vqovXY/Q4b+rNjgiheBsA/vbWA3IVvsS8lkQSX1gQ4ggSJx38oI2vREZXpTzhAv6tNUaX81E7QBBzkpDQayA==' );
 momentScript.setAttribute( 'crossorigin', 'anonymous' );
 momentScript.setAttribute( 'referrerpolicy', 'no-referrer' );
 document.head.appendChild( momentScript );
@@ -21,5 +21,5 @@ setInterval( function () {
 		if ( isNaN( Number( t.contextTrack.metadata.duration ) ) ) return true;
 		totalTime += Number( t.contextTrack.metadata.duration );
 	} );
-	document.querySelector( `h2.OSrSKCORKBO4JZclYmQ5` )?.style.setProperty( '--queue-remaining', `'${moment.utc( totalTime + Spicetify.Player.getDuration() - Spicetify.Player.getProgress() ).format( 'HH:mm:ss' )} Remaining'` );
+	document.querySelector( `h2.jf2HafzDEI9jn7Yo05eM` )?.style.setProperty( '--queue-remaining', `'${moment.utc( totalTime + Spicetify.Player.getDuration() - Spicetify.Player.getProgress() ).format( 'HH:mm:ss' )} Remaining'` );
 }, 1000 );
