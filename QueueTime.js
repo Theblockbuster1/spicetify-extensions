3f3f49897aa31aa2dcf13635b4e872ec548e9101
Theblockbuster1 <48650812+Theblockbuster1@users.noreply.github.com>
2022-09-10 14:32:25 +0100

    Queue title element + update moment.js

--- QueueTime.js
+++ QueueTime.js
@@ -1,16 +1,22 @@
 let qt_style = document.createElement( "style" );
 qt_style.innerHTML = `
-h2.jf2HafzDEI9jn7Yo05eM.standalone-ellipsis-one-line::after {
+h1.Type__TypeElement-goli3j-0.dnNHjd.DG9CsoFIptJhAneKoo_F {
+	position: relative;
+}
+h1.Type__TypeElement-goli3j-0.dnNHjd.DG9CsoFIptJhAneKoo_F::after {
     content: var(--queue-remaining);
-    float: right;
     color: gray;
+	font-size: 1rem;
+    position: absolute;
+    bottom: 0;
+    right: 0;
 }
 `;
 document.head.appendChild( qt_style );
 
 let momentScript = document.createElement( "script" );
-momentScript.setAttribute( 'src', 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.3/moment.min.js' );
-momentScript.setAttribute( 'integrity', 'sha512-dGM81hdEjiW5IomkknOx3fIfwij3c7xwh6TcrbumlkHJtO81OKNjLISJaPEhCgVxM6+0uGJ7KRmI2YJFn0AmHQ==' );
+momentScript.setAttribute( 'src', 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js' );
+momentScript.setAttribute( 'integrity', 'sha512-+H4iLjY3JsKiF2V6N366in5IQHj2uEsGV7Pp/GRcm0fn76aPAk5V8xB6n8fQhhSonTqTXs/klFz4D0GIn6Br9g==' );
 momentScript.setAttribute( 'crossorigin', 'anonymous' );
 momentScript.setAttribute( 'referrerpolicy', 'no-referrer' );
 document.head.appendChild( momentScript );
@@ -21,5 +27,5 @@ setInterval( function () {
 		if ( isNaN( Number( t.contextTrack.metadata.duration ) ) ) return true;
 		totalTime += Number( t.contextTrack.metadata.duration );
 	} );
-	document.querySelector( `h2.jf2HafzDEI9jn7Yo05eM` )?.style.setProperty( '--queue-remaining', `'${moment.utc( totalTime + Spicetify.Player.getDuration() - Spicetify.Player.getProgress() ).format( 'HH:mm:ss' )} Remaining'` );
+	document.querySelector( `h1.Type__TypeElement-goli3j-0.dnNHjd.DG9CsoFIptJhAneKoo_F` )?.style.setProperty( '--queue-remaining', `'${moment.utc( totalTime + Spicetify.Player.getDuration() - Spicetify.Player.getProgress() ).format( 'HH:mm:ss' )} Remaining'` );
 }, 1000 );
