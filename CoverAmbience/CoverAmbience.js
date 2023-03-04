394b523cf5192c3c93b799a34240e2baa8251a1c
Theblockbuster1 <48650812+Theblockbuster1@users.noreply.github.com>
2023-03-04 17:59:35 +0000

    More specific custom property name

--- CoverAmbience/CoverAmbience.js
+++ CoverAmbience/CoverAmbience.js
@@ -10,7 +10,7 @@ ca_style.innerHTML = `
     right: 0;
     width: 100%;
     height: 100%;
-    background-color: var(--cover-color);
+    background-color: var(--cover-ambience-color);
     -webkit-mask-image: linear-gradient(to right, #000f 0, #0000 280px, #0000 100%);
 }
 .main-nowPlayingBar-container::before {
@@ -24,7 +24,7 @@ vibrantScript.setAttribute('src', 'https://cdn.jsdelivr.net/npm/node-vibrant/dis
 document.head.appendChild(vibrantScript);
 
 async function setGradient(player) {
-    document.querySelector('.main-nowPlayingBar-container')?.style.setProperty('--cover-color', ((await Vibrant.from(`https://i.scdn.co/image/${player.data.track.metadata.image_large_url.split(':')[2]}`).getPalette()).Vibrant.hex || '#FFFFFF')+'69');
+    document.querySelector('.main-nowPlayingBar-container')?.style.setProperty('--cover-ambience-color', ((await Vibrant.from(`https://i.scdn.co/image/${player.data.track.metadata.image_large_url.split(':')[2]}`).getPalette()).Vibrant.hex || '#FFFFFF')+'69');
 }
 
 
