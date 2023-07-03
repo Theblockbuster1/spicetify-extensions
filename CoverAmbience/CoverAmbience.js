1390fb2a9398d6900a130b604ccf228cf829e6df
Theblockbuster1 <48650812+Theblockbuster1@users.noreply.github.com>
2023-07-03 17:19:20 +0100

    Cover Ambience: Edit Border Opacity, closes #21

--- CoverAmbience/CoverAmbience.js
+++ CoverAmbience/CoverAmbience.js
@@ -18,6 +18,8 @@ ca_style.innerHTML = `
     background-image: var(--bg-img) !important;
     position: relative;
     z-index: 100;
+    --cover-ambience-border-opacity: ${localStorage.CoverAmbienceBorderOpacity || 0.5};
+    --cover-ambience-border-opacity-small: ${((localStorage.CoverAmbienceBorderOpacity || 0.5) * 80) / 100};
 }
 .main-nowPlayingBar-container:before {
     background-image: var(--bg-img-before);
@@ -37,11 +39,11 @@ ca_style.innerHTML = `
 /* Add outlines to song text/artist/genre */
 .Root__now-playing-bar .main-trackInfo-name
 {
-  text-shadow: -1px -1px 0 rgba(var(--spice-rgb-player), 0.5), 1px -1px 0 rgba(var(--spice-rgb-player), 0.5), -1px 1px 0 rgba(var(--spice-rgb-player), 0.5), 1px 1px 0 rgba(var(--spice-rgb-player), 0.5);
+  text-shadow: -1px -1px 0 rgba(var(--spice-rgb-player), var(--cover-ambience-border-opacity)), 1px -1px 0 rgba(var(--spice-rgb-player), var(--cover-ambience-border-opacity)), -1px 1px 0 rgba(var(--spice-rgb-player), var(--cover-ambience-border-opacity)), 1px 1px 0 rgba(var(--spice-rgb-player), var(--cover-ambience-border-opacity));
 }
 .Root__now-playing-bar .main-trackInfo-artists, .Root__now-playing-bar .main-trackInfo-genres
 {
-  text-shadow: -1px -1px 0 rgba(var(--spice-rgb-player), 0.4), 1px -1px 0 rgba(var(--spice-rgb-player), 0.4), -1px 1px 0 rgba(var(--spice-rgb-player), 0.4), 1px 1px 0 rgba(var(--spice-rgb-player), 0.4);
+  text-shadow: -1px -1px 0 rgba(var(--spice-rgb-player), var(--cover-ambience-border-opacity-small)), 1px -1px 0 rgba(var(--spice-rgb-player), var(--cover-ambience-border-opacity-small)), -1px 1px 0 rgba(var(--spice-rgb-player), var(--cover-ambience-border-opacity-small)), 1px 1px 0 rgba(var(--spice-rgb-player), var(--cover-ambience-border-opacity-small));
 }
 `;
 document.head.appendChild(ca_style);
@@ -152,6 +154,82 @@ function initiate() {
   setGradient();
   Spicetify.Player.addEventListener('songchange', setGradient);
   setInterval(checkBackgroundColor, 5000);
+
+  window.updateCABorderOpacity = function(value) {
+    let style = document.querySelector('.main-nowPlayingBar-container')?.style;
+    style.setProperty('--cover-ambience-border-opacity', value / 100);
+    style.setProperty('--cover-ambience-border-opacity-small', ((value / 100) * 80) / 100);
+
+    localStorage.CoverAmbienceBorderOpacity = value / 100;
+  }
+
+  const info = document.querySelector("div.main-nowPlayingWidget-trackInfo.main-trackInfo-container");
+
+  var contextMenu = 0;
+
+  info.addEventListener('contextmenu', () => { // check if the context menu was clicked within the track info container
+    contextMenu = Date.now();
+  })
+
+  let borderOpacityMenuItem = new Spicetify.ContextMenu.Item(
+    "Edit Border Opacity",
+    () => {
+      Spicetify.PopupModal.display({
+        title: 'Border Opacity %',
+        content: `<style>
+          #coverAmbienceBorderRange {
+            width: calc(100% - 78px);
+            accent-color: var(--spice-button-active);
+          }
+          #coverAmbienceBorderNumber {
+            width: 69px;
+            padding: 11px;
+            border-radius: 16px;
+          }
+          #coverAmbienceBorderNumber::-webkit-inner-spin-button {
+            -webkit-appearance: auto;
+          }
+          .GenericModal__overlay {
+            background: none;
+          }
+          .main-embedWidgetGenerator-container {
+            box-shadow: 0 4px 13px rgb(var(--spice-rgb-shadow));
+          }
+          .x-searchInput-searchInputInput {
+            background-color: var(--spice-main-elevated);
+            border: 0;
+            border-radius: 500px;
+            color: var(--spice-text)!important;
+            height: 48px;
+            padding: 6px 36px;
+            text-overflow: ellipsis;
+            width: 100%
+          }
+          .x-searchInput-searchInputInput:hover {
+            background-color: var(--spice-highlight-elevated);
+            -webkit-box-shadow: 0 0 0 1px rgba(var(--spice-rgb-selected-row),.2);
+            box-shadow: 0 0 0 1px rgba(var(--spice-rgb-selected-row),.2);
+            outline: none
+          }
+          .x-searchInput-searchInputInput:focus {
+            -webkit-box-shadow: 0 0 0 2px var(--spice-text);
+            box-shadow: 0 0 0 2px var(--spice-text);
+            outline: none
+          }
+        </style>
+        <input id="coverAmbienceBorderRange" type="range" min="0" max="100" step="0.5" value="${(localStorage.CoverAmbienceBorderOpacity || 0.5) * 100}" oninput="this.nextElementSibling.value = this.value; updateCABorderOpacity(this.value)">
+        <input id="coverAmbienceBorderNumber" type="number" class="Type__TypeElement-sc-goli3j-0 TypeElement-mesto-type x-searchInput-searchInputInput" maxlength="5" autocorrect="off" autocapitalize="off" spellcheck="false" placeholder="50" data-testid="search-input" data-encore-id="type" min="0" max="100" step="0.5" value="${(localStorage.CoverAmbienceBorderOpacity || 0.5) * 100}" oninput="this.previousElementSibling.value=this.value; updateCABorderOpacity(this.value)">`,
+        isLarge: true
+      });
+    },
+    () => {
+      return Date.now() - contextMenu <= 150;
+    },
+    'edit',
+    false,
+  );
+  
+  borderOpacityMenuItem.register();
 }
 
 if (document.querySelector('.main-nowPlayingBar-container')) {
